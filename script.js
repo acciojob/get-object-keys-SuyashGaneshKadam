01a0691a3e4b405cd47148dd458d3d192a147312
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-11-20 18:05:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let student = {
+	name: "Suyash";
+}
+
+Object.prototype.getKeys = function(){
+	let ans = [];
+	for(i in student){
+		ans.push(i);
+	}
+	return ans;
+}
